9a5a08b9825ed23c6f862aaf19d424cb257a6b70
hemantrajurkar123 <hemant.rajurkar123@gmail.com>
2026-08-17 20:16:52 +0530

    intial commits of sql scripts

---
+++ creating_table.sql
@@ -0,0 +1,9 @@
+use company;
+CREATE TABLE employees (
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(100),
+department VARCHAR(50),
+salary DECIMAL(10,2)
+);
+
+SELECT * FROM employees;
---
+++ insert_values.sql
@@ -0,0 +1,20 @@
+use company;
+SELECT * FROM employees;
+-- source-W3 schools
+-- option 1-Write the table with specifying column names
+INSERT INTO employees (id,name,department,salary)
+VALUES(1,"Hemant","Analytics",100000);
+
+
+-- option 2- without specifying column names
+INSERT INTO employees VALUES (2,"Nisha","Electrical",50000);
+
+
+-- Adding multiple values
+-- If the column is auto incremant, we do not have to specify number in each values
+INSERT INTO employees (name,department,salary)
+VALUES 
+("Rohit","Marketing",60000),
+("Anushree","Software",40000),
+("Sagar","Coding",150000),
+("Pooja","Testing",35000);
